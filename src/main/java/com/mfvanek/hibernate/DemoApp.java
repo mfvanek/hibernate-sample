1d700c215fa55c35dc4645d4a4a5da048e7c084c
Ivan Vakhrushev <mfvanek@gmail.com>
2018-12-02 20:52:17 +0300

    Adding items into database using thread pool

--- src/main/java/com/mfvanek/hibernate/DemoApp.java
+++ src/main/java/com/mfvanek/hibernate/DemoApp.java
@@ -11,11 +11,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class DemoApp {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApp.class);
     private static SessionFactory sessionFactory;
+    private static final int LOOP_COUNT = 1_000;
+
     /**
      * There is another approach
      * https://docs.jboss.org/hibernate/orm/current/userguide/html_single/Hibernate_User_Guide.html#multitenacy
@@ -25,7 +30,9 @@ public class DemoApp {
     public static void main(String[] args) {
         try {
             init();
-            saveItem();
+            saveFromCurrentThread();
+            saveFromNewSingleThread();
+            saveUsingThreadPool();
             countTotal();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
@@ -70,6 +77,59 @@ public class DemoApp {
         try (Session session = sessionFactory.openSession()) {
             Long rowsCount = session.createQuery("select count(*) from TestEvent", Long.class).getSingleResult();
             System.out.println("rows count = " + (rowsCount != null ? rowsCount : 0));
+        }
+    }
+
+    private static void saveFromCurrentThread() {
+        new DataSaver("current thread").run();
+    }
+
+    private static void saveFromNewSingleThread() {
+        try {
+            final Thread thread = new Thread(new DataSaver("new single thread"), "pg_single");
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private static void saveUsingThreadPool() {
+        final String message = "Saving items using thread pool";
+        System.out.println(message);
+        final long timeStart = System.currentTimeMillis();
+        try {
+            final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
+            // final ExecutorService threadPool = Executors.newFixedThreadPool(10);
+            for (int i = 0; i < LOOP_COUNT; ++i) {
+                threadPool.submit(DemoApp::saveItem);
+            }
+            threadPool.shutdown();
+            threadPool.awaitTermination(10L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+        }
+        System.out.println(String.format("%s. Completed. Time elapsed = %d (ms)",
+                message, System.currentTimeMillis() - timeStart));
+    }
+
+    private static class DataSaver implements Runnable {
+
+        private final String message;
+
+        DataSaver(String message) {
+            this.message = "Saving items from " + message;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(this.message);
+            final long timeStart = System.currentTimeMillis();
+            for (int i = 0; i < LOOP_COUNT; ++i) {
+                saveItem();
+            }
+            System.out.println(String.format("%s. Completed. Time elapsed = %d (ms)",
+                    this.message, System.currentTimeMillis() - timeStart));
         }
     }
 }
