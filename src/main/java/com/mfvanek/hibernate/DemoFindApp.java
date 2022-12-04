c0f9bc40aa8d16ea68809b1600148df5fb72fe07
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2022-12-04 15:04:39 +0300

    Added static code analysis

--- src/main/java/com/mfvanek/hibernate/DemoFindApp.java
+++ src/main/java/com/mfvanek/hibernate/DemoFindApp.java
@@ -11,32 +11,24 @@ import org.hibernate.Transaction;
 @Slf4j
 public class DemoFindApp {
 
-    private static SessionFactory sessionFactory;
-
-    public static void main(String[] args) {
-        try {
-            sessionFactory = SessionFactoryUtil.build();
-            final TestEvent first = findById(11L);
+    public static void main(final String[] args) {
+        try (SessionFactory sessionFactory = SessionFactoryUtil.build()) {
+            final TestEvent first = findById(sessionFactory, 11L);
             log.info("Inside main: {}", first);
             // First query is for reading event, and the second one is for reading info
             SessionFactoryUtil.validateQueriesCount(2);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
-        } finally {
-            if (sessionFactory != null) {
-                sessionFactory.close();
-                sessionFactory = null;
-            }
         }
     }
 
-    private static TestEvent findById(Long id) {
+    private static TestEvent findById(final SessionFactory sessionFactory, final long id) {
         TestEvent result = new TestEvent();
         try (Session session = sessionFactory.openSession()) {
-            Transaction trn = session.beginTransaction();
+            final Transaction trn = session.beginTransaction();
             try {
                 result = session.get(TestEvent.class, id);
-                System.out.println("Inside Session: " + result);
+                log.trace("Inside Session: {}", result);
                 // In order to avoid LazyInitializationException
                 Hibernate.initialize(result);
                 trn.commit();
