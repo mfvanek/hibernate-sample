c902b2002f60a22f958743db81c90e964c6935ff
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-06-03 22:16:27 +0400

    Migration to Gradle (#10)
    * Migrated to JDK 19 in CI
    
    * Added Gradle wrapper
    
    * Migrated to Gradle
    
    * Fix links
    
    ---------
    
    Co-authored-by: i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>

---
+++ build.gradle.kts
@@ -0,0 +1,165 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.SpotBugsTask
+import net.ltgt.gradle.errorprone.errorprone
+
+plugins {
+    id("java-library")
+    id("jacoco")
+    id("maven-publish")
+    id("checkstyle")
+    id("pmd")
+    id("com.github.spotbugs") version "5.0.14"
+    id("net.ltgt.errorprone") version "3.1.0"
+    id("io.freefair.lombok") version "8.0.1"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+group = "com.mfvanek"
+version = "1.4.0-SNAPSHOT"
+description = "Hibernate sample app"
+
+dependencies {
+    implementation("org.hibernate.orm:hibernate-core:6.1.5.Final")
+    implementation("org.hibernate.orm:hibernate-hikaricp:6.1.5.Final")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.postgresql:postgresql:42.5.4")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation("org.liquibase:liquibase-core:4.20.0")
+    implementation("org.mongodb:mongodb-driver:3.12.12")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+    checkstyle("com.thomasjensen.checkstyle.addons:checkstyle-addons:7.0.1")
+    errorprone("com.google.errorprone:error_prone_core:2.19.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withJavadocJar()
+    withSourcesJar()
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone {
+        disableWarningsInGeneratedCode.set(true)
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        dependsOn(checkstyleMain, checkstyleTest, pmdMain, pmdTest, spotbugsMain, spotbugsTest)
+        finalizedBy(jacocoTestReport, jacocoTestCoverageVerification)
+    }
+
+    javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    jacocoTestCoverageVerification {
+        dependsOn(jacocoTestReport)
+        violationRules {
+            rule {
+                limit {
+                    counter = "CLASS"
+                    value = "MISSEDCOUNT"
+                    maximum = "12".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "METHOD"
+                    value = "MISSEDCOUNT"
+                    maximum = "67".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "LINE"
+                    value = "MISSEDCOUNT"
+                    maximum = "253".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "BRANCH"
+                    value = "COVEREDRATIO"
+                    minimum = "0.0".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "INSTRUCTION"
+                    value = "COVEREDRATIO"
+                    minimum = "0.02".toBigDecimal()
+                }
+            }
+        }
+    }
+
+    check {
+        dependsOn(jacocoTestCoverageVerification)
+    }
+}
+
+lombok {
+    version.set(libs.versions.lombok.get())
+}
+
+jacoco {
+    toolVersion = libs.versions.jacoco.get()
+}
+
+checkstyle {
+    toolVersion = libs.versions.checkstyle.get()
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+pmd {
+    toolVersion = libs.versions.pmd.get()
+    isConsoleOutput = true
+    ruleSetFiles = files("config/pmd/pmd.xml")
+    ruleSets = listOf()
+}
+
+spotbugs {
+    showProgress.set(true)
+    effort.set(Effort.MAX)
+    reportLevel.set(Confidence.LOW)
+    excludeFilter.set(file("config/spotbugs/exclude.xml"))
+}
+tasks.withType<SpotBugsTask>().configureEach {
+    reports {
+        create("xml") { enabled = true }
+        create("html") { enabled = true }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
