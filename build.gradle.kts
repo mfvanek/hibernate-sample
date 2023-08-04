b1b4d0b9b5fa8817d099576db4a33834223e64e3
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-08-04 16:04:45 +0400

    Updated Gradle to 8.2.1 (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("maven-publish")
     id("checkstyle")
     id("pmd")
-    id("com.github.spotbugs") version "5.0.14"
+    id("com.github.spotbugs") version "5.1.1"
     id("net.ltgt.errorprone") version "3.1.0"
     id("io.freefair.lombok") version "8.1.0"
     id("com.github.ben-manes.versions") version "0.47.0"
@@ -43,7 +43,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.24.2")
 
     checkstyle("com.thomasjensen.checkstyle.addons:checkstyle-addons:7.0.1")
-    errorprone("com.google.errorprone:error_prone_core:2.20.0")
+    errorprone("com.google.errorprone:error_prone_core:2.21.0")
 }
 
 java {
